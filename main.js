ba1cf760d972e5c8deeeddaae7702ed7f9410544
dangvh <dangvh@bravebits.vn>
2024-04-23 15:42:24 +0700

    init project

---
+++ main.js
@@ -0,0 +1,43 @@
+import * as THREE from "three";
+
+async function main() {
+  const { camera, scene, renderer} = setUpScene();
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  // Start animation
+  requestAnimationFrame(function animate() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+  })
+}
+
+
+function setUpScene() {
+  const scene = new THREE.Scene();
+
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.z = 5;
+
+  const canvas = document.getElementById("canvas#1");
+  const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  return {
+    scene,
+    camera,
+    renderer,
+  };
+}
+
+main()
